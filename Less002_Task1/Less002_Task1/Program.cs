351dfc23d3d18f2f88a09ea86b339966bc260f53
Олександр Дибач <alex.dybach.2014@i.ua>
2020-10-27 13:51:15 +0200

    Done lesson 2 task 1

---
+++ Less002_Task1/Less002_Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Less002_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long birthYear = 2000;
+            Console.WriteLine($"Birth year: {birthYear}");
+            const short myVar1 = 87;
+            long myVar2 = 32788;
+            short myVar3 = (short)myVar2;
+        }
+    }
+}
